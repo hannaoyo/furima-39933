91f288da773ac508671cebf1b8fcb548338e0f82
hannaoyo <hannaoyo11@gmail.com>
2024-01-07 21:09:27 +0900

    パスワード必須、6文字以上、半角英数字混合、一致

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  #validates :password, presence: true
-  #validates :password_confirmation, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/ }
+  validates :password_confirmation, presence: true
   #validates :first_name, presence: true
   #validates :last_name, presence: true
   #validates :first_name_kana, presence: true
