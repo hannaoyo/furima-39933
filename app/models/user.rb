272cb11ca92dd4f75c0a663b598bb9a12dd13f34
hannaoyo <hannaoyo11@gmail.com>
2024-01-07 21:24:30 +0900

    名前カナ(全角)必須、全角（カタカナ）

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true
   validates :first_name, presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name, presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  #validates :first_name_kana, presence: true
-  #validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   #validates :birthday, presence: true
 
 end
