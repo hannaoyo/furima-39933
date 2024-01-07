19326eee5ea2bbbcfb7c5862618fc1ed66707517
hannaoyo <hannaoyo11@gmail.com>
2024-01-07 21:17:31 +0900

    名前(全角)必須、全角（漢字・ひらがな・カタカナ）

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/ }
   validates :password_confirmation, presence: true
-  #validates :first_name, presence: true
-  #validates :last_name, presence: true
+  validates :first_name, presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   #validates :first_name_kana, presence: true
   #validates :last_name_kana, presence: true
   #validates :birthday, presence: true
