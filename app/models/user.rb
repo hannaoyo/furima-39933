d2c9f7331195fbf2b1729c7ac12130e733aca418
hannaoyo <hannaoyo11@gmail.com>
2024-01-08 20:01:08 +0900

    不要なバリデーション削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/ }
-  validates :password_confirmation, presence: true
   validates :first_name, presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name, presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
