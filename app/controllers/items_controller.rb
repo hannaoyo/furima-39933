07b9c83f19eeff21611e1f735d83ac1385de44fd
hannaoyo <hannaoyo11@gmail.com>
2024-01-14 21:58:08 +0900

    商品情報をテーブル保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,20 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      render :index, status: :unprocessable_entity
+  end
+
+
   private
 
+
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_detail, :price).merge(user_id: current_user.id)
   end
 
 end
