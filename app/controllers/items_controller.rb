4d773472a05649886af7bcddf54f4411d6ff7784
hannaoyo <hannaoyo11@gmail.com>
2024-01-14 21:52:33 +0900

    商品出品ページ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,16 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
   end
 
   private
 
-  def items_params
+  def item_params
     params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
-  
+
 end
