3ac593c1cdb2f35e54dcfe7bf3a133a6356688c9
hannaoyo <hannaoyo11@gmail.com>
2024-01-17 22:09:13 +0900

    indexアクションをコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,9 @@ class ItemsController < ApplicationController
   #ログインしていないユーザーはログインページに移動
   before_action :authenticate_user!,  except: [:index]
 
-  def index
-    @items = Item.includes(:user).order("created_at DESC")
-  end
+  #def index
+   # @items = Item.includes(:user).order("created_at DESC")
+  #end
 
   def new
     @item = Item.new
