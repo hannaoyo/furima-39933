510b8f9ec3db980c30fcfaa4266b04780017caa4
hannaoyo <hannaoyo11@gmail.com>
2024-01-30 17:50:29 +0900

    createアクション設定・フォーム情報を保存できる処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,5 +6,18 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :address_number, :building_name, :phone_number).merge(user_id: current_user.id)
   end
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,2 @@
-module PuschaseRecordsHelper
+module OrdersHelper
 end
