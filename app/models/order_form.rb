281f82015dd2fd32b56d91ef96047926354d69f2
hannaoyo <hannaoyo11@gmail.com>
2024-02-01 15:11:13 +0900

    テストコード実装

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -8,7 +8,7 @@ class OrderForm
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
-    validates :building_name
+    validates :address_number
     validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
   end
 
