6d6ba80e9531bc2be54de5f633624faed4133a34
hannaoyo <hannaoyo11@gmail.com>
2024-02-14 18:50:08 +0900

    建物名のカラム修正

--- db/migrate/20240128114916_create_delivery_infos.rb
+++ db/migrate/20240128114916_create_delivery_infos.rb
@@ -6,7 +6,7 @@ class CreateDeliveryInfos < ActiveRecord::Migration[7.0]
       t.integer    :prefecture_id,   null: false
       t.string     :city,            null: false
       t.string     :address_number,  null: false
-      t.string     :building_name,   null: false
+      t.string     :building_name
       t.string     :phone_number,    null: false
       t.timestamps
     end
