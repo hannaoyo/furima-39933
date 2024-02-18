2c1777da337d4bcce8266c44e34b84d7d64cad1e
hannaoyo <hannaoyo11@gmail.com>
2024-02-18 20:50:38 +0900

    販売価格のJavaScript編集

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
@@ -9,4 +9,7 @@ window.addEventListener('load', () => {
     //販売利益
     ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
   })
-});
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
